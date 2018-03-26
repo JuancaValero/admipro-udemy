3d39a35f0bc7ec0389dbf5a6a8454c0035da260e
Valerus <juancavalerog@gmail.com>
2018-03-25 19:31:02 -0500

    Seccion 8 terminada

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -11,6 +11,8 @@ export class SidebarService {
         { titulo: 'Dashboard', url: '/dashboard' },
         { titulo: 'ProgressBar', url: '/progress' },
         { titulo: 'Graficas', url: '/grafica1' },
+        { titulo: 'Promesas', url: '/promesas' },
+        { titulo: 'RxJs', url: '/rxjs' }
       ]
     }
   ];
